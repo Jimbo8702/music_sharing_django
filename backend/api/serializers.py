ce1d520e79be7622ea433010fb57d0bc30c1faea
James Sgarella <Jimbo8702@gmail.com>
2022-05-10 13:21:06 -0400

    env variables fixed, first view loaded

--- backend/api/serializers.py
+++ backend/api/serializers.py
@@ -5,4 +5,11 @@ from .models import Room
 class RoomSerializer(serializers.ModelSerializer):
     class Meta:
         model = Room
-        fields = ("id", "code", "host", "guest_can_pause", "votes_to_skip")
+        fields = (
+            "id",
+            "code",
+            "host",
+            "guest_can_pause",
+            "vote_to_skip",
+            "created_at",
+        )

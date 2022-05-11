ad7bb528733b4fb8c3a19d65941e266da6354dc4
James Sgarella <Jimbo8702@gmail.com>
2022-05-11 12:36:52 -0400

    routing issues and auto join prejoined room added

--- project/api/views.py
+++ project/api/views.py
@@ -5,7 +5,7 @@ from rest_framework.views import APIView
 from rest_framework.response import Response
 from .serializers import RoomSerializer, CreateRoomSerializer
 from .models import Room
-
+from django.http import JsonResponse
 # Create your views here.
 class RoomView(generics.ListAPIView):
     queryset = Room.objects.all()
@@ -85,3 +85,12 @@ class CreateRoomView(APIView):
         return Response(
             {"Bad Request": "Invalid data..."}, status=status.HTTP_400_BAD_REQUEST
         )
+
+class UserInRoom(APIView):
+    def get(self, request, format=None):
+        if not self.request.session.exists(self.request.session.session_key):
+            self.request.session.create()
+        data = {
+            'code': self.request.session.get('room_code')
+        }
+        return JsonResponse(data, status=status.HTTP_200_OK)
--- project/frontend/src/components/HomePage.js
+++ project/frontend/src/components/HomePage.js
@@ -1,21 +1,69 @@
-import React, { Component } from "react";
+import React, { useEffect, useState } from "react";
 import JoinRoomPage from "./JoinRoomPage";
 import CreateRoomPage from "./CreateRoomPage";
-
+import { useNavigate } from "react-router-dom";
+import { Grid, Button, ButtonGroup, Typography } from "@material-ui/core";
 import Room from "./Room";
 import {
   BrowserRouter as Router,
   Routes,
   Route,
   Link,
-  Redirect,
+  Navigate,
 } from "react-router-dom";
 
 export default function HomePage() {
+  const [homeState, setHomeState] = useState({
+    roomCode: null,
+  });
+
+  useEffect(async () => {
+    await fetch("/api/user-in-room")
+      .then((response) => response.json())
+      .then((data) => {
+        console.log(data);
+        setHomeState({
+          roomCode: data.code,
+        });
+      });
+  }, []);
+
+  function Home() {
+    return (
+      <Grid container spacing={3}>
+        <Grid item xs={12} align="center">
+          <Typography variant="h3" compact="h3">
+            House Party
+          </Typography>
+        </Grid>
+        <Grid item xs={12} align="center">
+          <ButtonGroup disableElevation variant="contained" color="primary">
+            <Button color="primary" to="/join" component={Link}>
+              Join a Room
+            </Button>
+            <Button color="secondary" to="/create" component={Link}>
+              Create a Room
+            </Button>
+          </ButtonGroup>
+        </Grid>
+      </Grid>
+    );
+  }
+
   return (
     <Router>
       <Routes>
-        <Route exact path="/" element={<h1> this is home </h1>} />
+        <Route
+          exact
+          path="/"
+          element={
+            homeState.roomCode ? (
+              <Navigate to={`/room/${homeState.roomCode}`} />
+            ) : (
+              <Home />
+            )
+          }
+        />
         <Route path="/join" element={<JoinRoomPage />} />
         <Route path="/create" element={<CreateRoomPage />} />
         <Route path="/room/:roomCode" element={<Room />} />
